53a8e97a10859a1ce2302a35e90c1a05e06903e9
Bibarel <80209481+GZGTE@users.noreply.github.com>
2022-02-20 02:19:10 +0900

    Add files via upload

--- js/background.js
+++ js/background.js
@@ -3,10 +3,4 @@ const images = ["0.jpg", "1.jpg", "2.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-//const bgImage = document.createElement("img");
-
-//bgImage.src = `img/${chosenImage}`;
-
-//document.body.appendChild(bgImage);
-
 document.body.style= `background-image: url("img/${chosenImage}");background-size: cover;`;
